16b0374744c46a72a9ad2f2e2d65054fe8e079e9
Delvinsulistio <delvinsulistio@gmail.com>
2025-06-04 09:55:08 +0700

    Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,1 +1,23 @@
-wmwww
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'article_id', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
